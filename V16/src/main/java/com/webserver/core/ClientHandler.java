793e1cd00eb5018e5763944ed49e3f3b324a1b95
starkunpyf <2560331078@qq.com>
2022-02-23 09:53:15 +0800

    V16

---
+++ V16/src/main/java/com/webserver/core/ClientHandler.java
@@ -0,0 +1,48 @@
+package com.webserver.core;
+
+import com.webserver.http.EmptyRequestException;
+import com.webserver.http.HttpServletPesponse;
+import com.webserver.http.HttpServletRequst;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class ClientHandler implements Runnable{
+    private Socket socket;
+
+    public ClientHandler(Socket socket){
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            //解析请求
+            HttpServletRequst requst = new HttpServletRequst(socket);
+            HttpServletPesponse response = new HttpServletPesponse(socket);
+            //处理请求
+            DispatcherServlet servlet = new DispatcherServlet();
+            servlet.service(requst,response);
+            //发送响应
+            response.Response();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (EmptyRequestException e) {
+
+        } finally{
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
